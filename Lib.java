622aea9b2a923b2e7781ad9ff0e5ddf2552e6313
Daniel Martín Millet <danielsivale@gmail.com>
2017-03-15 20:44:46 +0100

    Modifico en metodo3

--- Lib.java
+++ Lib.java
@@ -10,14 +10,14 @@ public class Lib {
     }
 
     public void metodoAlumno2(){
-        System.out.println("metodo 2");
+        metodo3());
     }
 
-    public  void  metodo3 (){
+    public  void  metodo3(){
       System.out.println("metodo 3");
     }
 
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("Cambio en metodo 3");
     }
 }
