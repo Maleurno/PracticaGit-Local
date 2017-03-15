ea72e79582e7a679208476cd3c31b30e28439ef6
Daniel Martín Millet <danielsivale@gmail.com>
2017-03-15 20:39:10 +0100

    Modifico Lib.java

--- Lib.java
+++ Lib.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Daniel Martín Millet
+ * @author Daniel Martï¿½n Millet
  */
 
 public class Lib {
@@ -11,6 +11,10 @@ public class Lib {
 
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+    }
+
+    public  void  metodo3 (){
+      System.out.println("metodo 3");
     }
 
     public void metodoComunitario(){
